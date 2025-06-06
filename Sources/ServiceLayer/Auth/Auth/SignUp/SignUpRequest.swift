81c7714c1a0fef02bad2bf7583e18c7a658c83f4
Nikolai Lipski <>
2025-06-06 16:34:59 -0300

    some upd

--- Sources/ServiceLayer/Auth/Auth/SignUp/SignUpRequest.swift
+++ Sources/ServiceLayer/Auth/Auth/SignUp/SignUpRequest.swift
@@ -13,12 +13,23 @@ public final class SignUpRequest: BaseRequest {
     public struct Signature {
         public let timestamp: Int
         public let signature: String
+        
+        public init(timestamp: Int, signature: String) {
+            self.timestamp = timestamp
+            self.signature = signature
+        }
     }
     
     public struct Params {
         public let queries: Query
         public let timestamp: Int
         public let signature: String
+        
+        public init(queries: Query, timestamp: Int, signature: String) {
+            self.queries = queries
+            self.timestamp = timestamp
+            self.signature = signature
+        }
     }
     
     public var method: HTTP.Method {
