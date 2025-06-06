9dae54bf6f1132e49be05ad10e12f243bc4abcb7
Nikolai Lipski <>
2025-06-06 16:22:41 -0300

    some upd

--- Sources/ServiceLayer/Auth/Auth/SignUp/SignUpRequest+Model.swift
+++ Sources/ServiceLayer/Auth/Auth/SignUp/SignUpRequest+Model.swift
@@ -8,61 +8,29 @@
 import Foundation
 
 extension SignUpRequest {
-
-    struct Query: Codable {
-        let name: String
-        let surname: String
-        let gender: Int
-        let email: String
-        let password: String
-        let country: String
-        let prefix: String
-        let phone: String
-        let dob: String
-        let acceptPolicy: Bool
-        let recaptcha: String
-        let currency: String
-        let city: String
-        let street: String
-        let postcode: String
-        let provinceId: Int?
-        let btag : String?
-        let clickId : String?
-        let tracking: Tracking
+    public struct Query: Codable {
+        public let name: String
+        public let surname: String
+        public let gender: Int
+        public let email: String
+        public let password: String
+        public let country: String
+        public let prefix: String
+        public let phone: String
+        public let dob: String
+        public let acceptPolicy: Bool
+        public let recaptcha: String
+        public let currency: String
+        public let city: String
+        public let street: String
+        public let postcode: String
+        public let provinceId: Int?
+        public let btag : String?
+        public let clickId : String?
+        public let tracking: Tracking
     }
 }
 
-
-//let nickname: String?
-//let bonusType : Int
-//let gender : Int
-//let currency : String
-//let prefix : String
-//let country : String
-//let email : String
-//let promotion : Bool
-//let password : String
-//let phone : String
-//let personalCode : String?
-//let name : String
-//let surname : String
-//let dob : String
-//let day : String
-//let month : String
-//let year : String
-//let terms : Bool
-//var recaptcha : String
-//let btag : String?
-//let ctag : String?
-//let clickId : String?
-//let utmCampaign : String?
-//let utmContent : String?
-//let utmMedium : String?
-//let utmSource : String?
-//let utmTerm : String?
-//let langIso : String // en
-//let localeIso : String // en
-
-struct Tracking: Codable {
+public struct Tracking: Codable {
     let subid: String?
 }
