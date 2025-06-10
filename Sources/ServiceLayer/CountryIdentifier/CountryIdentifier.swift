85be40b8f2d25b81459f86ba233c47c5bf48ed47
Nikolai Lipski <>
2025-06-09 22:27:34 -0300

    added country identifier

---
+++ Sources/ServiceLayer/CountryIdentifier/CountryIdentifier.swift
@@ -0,0 +1,24 @@
+//
+//  CountryIdentifier.swift
+//  BetApp
+//
+//  Created by Nikolai Lipski on 6.05.25.
+//
+
+import Foundation
+
+public protocol CountryIdentifierProtocol {
+    func fetchUserCountry() async throws -> IPInfo
+}
+
+public final class CountryIdentifier: CountryIdentifierProtocol {
+    public func fetchUserCountry() async throws -> IPInfo {
+        let url = URL(string: "https://ipapi.co/json/")!
+
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let info = try JSONDecoder().decode(IPInfo.self, from: data)
+
+        return info
+    }
+}
+
---
+++ Sources/ServiceLayer/CountryIdentifier/IPInfo.swift
@@ -0,0 +1,12 @@
+//
+//  IPInfo.swift
+//  ServiceLayer
+//
+//  Created by Nikolai Lipski on 9.06.25.
+//
+import Foundation
+
+public struct IPInfo: Decodable {
+    public let country: String
+    public let country_name: String
+}
