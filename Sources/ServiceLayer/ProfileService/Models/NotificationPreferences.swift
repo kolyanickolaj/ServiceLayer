20264a3b0f28c9fcaafcb4ae6ce378676132c776
Nikolai Lipski <>
2025-06-24 18:47:16 -0300

    some upd

--- Sources/ServiceLayer/ProfileService/Models/NotificationPreferences.swift
+++ Sources/ServiceLayer/ProfileService/Models/NotificationPreferences.swift
@@ -13,4 +13,13 @@ public struct NotificationPreferences: Codable, JSONParsable {
         case isPhoneEnabled = "phone_marketing_allowed"
         case isEmailEnabled = "email_marketing_allowed"
     }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let isPhoneEnabledInt = try container.decode(Int.self, forKey: .isPhoneEnabled)
+        self.isPhoneEnabled = isPhoneEnabledInt == 1
+        let isEmailEnabledInt = try container.decode(Int.self, forKey: .isEmailEnabled)
+        self.isEmailEnabled = isEmailEnabledInt == 1
+    }
 }
