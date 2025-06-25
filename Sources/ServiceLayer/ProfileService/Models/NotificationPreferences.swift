bf5eda389e511262ffaddc0abc2fc26b98e84151
Nikolai Lipski <>
2025-06-25 15:51:38 -0300

    some update

--- Sources/ServiceLayer/ProfileService/Models/NotificationPreferences.swift
+++ Sources/ServiceLayer/ProfileService/Models/NotificationPreferences.swift
@@ -9,6 +9,14 @@ public struct NotificationPreferences: Codable, JSONParsable {
     public let isEmailEnabled: Bool
     public let isPhoneEnabled: Bool
     
+    public init(
+        isPhoneEnabled: Bool = false,
+        isEmailEnabled: Bool = false
+    ) {
+        self.isPhoneEnabled = isPhoneEnabled
+        self.isEmailEnabled = isEmailEnabled
+    }
+    
     enum CodingKeys: String, CodingKey {
         case isPhoneEnabled = "phone_marketing_allowed"
         case isEmailEnabled = "email_marketing_allowed"
